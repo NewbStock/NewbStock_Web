8cd426e4865931fd3decdf3329e4fa2b197a9d7b
out_of_anjoong <joongh0113@gmail.com>
2024-08-14 16:03:22 +0900

    Header 추가

--- newbstock_front/.next/static/chunks/webpack.js
+++ newbstock_front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e709ad2599af378"; }
+/******/ 		__webpack_require__.h = function() { return "85567b1ee38ab6f4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- newbstock_front/.next/static/webpack/webpack.809f192dd0acda75.hot-update.js
+++ newbstock_front/.next/static/webpack/webpack.809f192dd0acda75.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "5e709ad2599af378"; }
+/******/ 	__webpack_require__.h = function() { return "d3d4db4522655eea"; }
 /******/ }();
 /******/ 
 /******/ }
