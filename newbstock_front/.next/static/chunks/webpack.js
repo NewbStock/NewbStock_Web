eaa1682641171a893c3e58e59eadeabc958f6a95
out_of_anjoong <joongh0113@gmail.com>
2024-08-16 17:36:25 +0900

    NewbieContent 임시 커밋

--- newbstock_front/.next/static/chunks/webpack.js
+++ newbstock_front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e36f2558f0226e0"; }
+/******/ 		__webpack_require__.h = function() { return "3d8353045322ee3a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- newbstock_front/components/NewbieContent/NewbieContent.jsx
+++ newbstock_front/components/NewbieContent/NewbieContent.jsx
@@ -5,10 +5,15 @@ function NewbieContent() {
     return (
         <ul className={styles.chart_list}>
             <li className={styles.item}>
-                <iframe src="" frameborder="0"></iframe>
+                <iframe
+                    src="http://43.202.160.91:8808/explore/?standalone=1&slice_id=1"
+                    frameborder="0"
+                    className={styles.chart}
+                >
+                </iframe>
             </li>
             <li className={styles.item}>
-                <iframe src="" frameborder="0"></iframe>
+                <iframe src="" frameborder="0" className={styles.chart}></iframe>
             </li>
         </ul>
     );
